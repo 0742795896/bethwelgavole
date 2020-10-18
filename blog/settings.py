3ac88317fb7278cbe8f9aa763ad397b18ad66e04
gavole <bethwelifedha@gmail.com>
2020-10-18 23:34:46 +0300

    h

--- blog/settings.py
+++ blog/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'k*b$$!%hiswnhp2pad)6(u4x_g)r5w%@&zlv&@2hrz4#f3+#57'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://beautifulblog.herokuapp.com/']
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
