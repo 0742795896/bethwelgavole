c91b1f633c544758a9645abec63cc721364a0ba1
gavole <bethwelifedha@gmail.com>
2020-10-18 22:46:59 +0300

    updated files

---
+++ posts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Author, Category, Post, Comment, PostView
+
+admin.site.register(Author)
+admin.site.register(Category)
+#admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(PostView)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields=['title', 'content', 'overview',]
+
+
+    
+    
