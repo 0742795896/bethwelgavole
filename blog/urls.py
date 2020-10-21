3c893a1605c83f7b10162e88a0183690e5baa2f1
gavole <bethwelifedha@gmail.com>
2020-10-21 10:06:56 +0300

    f

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 
 from posts.views import (
     index,
@@ -46,4 +47,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
 
-
+if not settings.DEBUG: urlpatterns += [ url(r'^uploads/(?P<path>.)$', serve,{'document_root': settings.MEDIA_ROOT}), url(r'^static/(?P<path>.)$', serve,{'document_root': settings.STATIC_ROOT}), ]
